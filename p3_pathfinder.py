16c9f8232bcec0eebe9d13feee7efa5ceba34da5
Dustin Ames Pfeiffer <dpfeiffe@be-linux22.lt.ucsc.edu>
2015-04-17 18:34:27 -0700

    can find source and dest

--- p3_pathfinder.py
+++ p3_pathfinder.py
@@ -1,3 +1,12 @@
 
 def find_path(source, destination, mesh):
-    print "no"
+    sx, sy = source
+    dx, dy = destination
+    visited = []
+    for box in mesh['boxes']:
+        x1, x2, y1, y2 = box
+        if x1 < sx and x2 > sx and y1 < sy and y2 > sy:
+            visited.append(box)
+        if x1 < dx and x2 > dx and y1 < dy and y2 > dy:
+            visited.append(box)
+    return [], visited
